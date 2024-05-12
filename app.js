10c9e28ecfb8f673b309547e83b891e6ea02c166
yasmeenrahimhuda <155715580+yasmeenrahimhuda@users.noreply.github.com>
2024-05-12 23:37:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// 1. Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+ prompt("City Name", "karachi")
+ alert("Welcome to the city of Lights");
+
+// 2 Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+ prompt ("gender" ,"male")
+ alert("Good Morning Sir");
+ prompt ("gender","female")
+ alert("Good Morning Ma'am");
+
+// 3. Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
+// Prompt the user to enter the color of the traffic signal
+
+
+// 4. Write a program to take input remaining fuel in car (in
+//     litres) from user. If the current fuel is less than 0.25litres,
+//     show the message “Please refill the fuel in your car”
+
+// 5. Run this script, & check whether alert message would be
+// displayed or not. Record the outputs.
+
+// 8. Write a program to check whether the given number is
+// divisible by 3. Show the message to the user if the number
+// is divisible by 3.
+
